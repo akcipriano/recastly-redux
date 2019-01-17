31ee91218e10b8725c74e804cce1b2a49e9b01f0
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-01-16 22:27:55 -0800

    Complete reducers

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,13 +1,24 @@
 import { combineReducers } from 'redux';
-import currentVideo from './currentVideo.js';
-import videoList from './videoList.js';
+import currentVideoReducer from './currentVideo.js';
+import videoListReducer from './videoList.js';
 
-var rootReducer = () => {};
+var rootReducer = (state = {}, action) => {
+  return {
+    currentVideo: currentVideoReducer(state.currentVideo, action),
+    videoList: videoListReducer(state.videos, action)
+    // combineReducers({ currentVideoReducer, videoListReducer})
+
+  };
+
+};
+
+// var rootReducer = combineReducers({ currentVideoReducer, videoListReducer});
 
 //TODO: define the root reducer for this app
 
 //HINT: you'll need to combine the other two reducers in this
 //  app into a single reducer using the 'combineReducers' method
 //  listed above.
+
 
 export default rootReducer;
