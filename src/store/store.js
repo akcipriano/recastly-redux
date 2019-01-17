bf9915a02ca5d6489d6d3c0271e4dd0452af3b82
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-01-16 19:45:53 -0800

    Complete planning state

--- src/store/store.js
+++ src/store/store.js
@@ -3,3 +3,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+//App.state.videos
+//App.state.currentVideo
+//Search.state.value
