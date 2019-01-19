acbb457123fdadf3a24f3bddac2532a5d943b3da
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-01-18 20:49:55 -0800

    Complete replacing components with containers

--- src/components/App.js
+++ src/components/App.js
@@ -9,42 +9,55 @@ import changeVideoList from '../actions/videoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import store from '../store/store.js';
 import Search from './Search.js';
+import YOUTUBE_API_KEY from '../config/youtube.js';
+import searchYouTube from '../lib/searchYouTube.js';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      videos: [],
-      currentVideo: null
-    };
+    // this.state = {
+    //   videos: [],
+    //   currentVideo: null
+    // };
   }
 
   componentDidMount() {
-    this.getYouTubeVideos('Koala');
-  }
-
-  handleVideoListEntryTitleClick(video) {
-    this.setState({currentVideo: video});
-  }
-
-  getYouTubeVideos(query) {
+    // this.getYouTubeVideos('Koala');
     var options = {
-      key: this.props.API_KEY,
-      query: query
+      key: YOUTUBE_API_KEY,
+      query: 'funny pets'
     };
 
-    this.props.searchYouTube(options, (videos) => {
-      this.setState({
-        videos: videos,
-        currentVideo: videos[0]
-      });
+    searchYouTube(options, (videos) => {
+      store.dispatch(changeVideoList(videos));
+      store.dispatch(changeVideo(videos[0]));
     });
+
+    // store.dispatch(changeVideoList(videos));
   }
 
-  handleSearchInputChange(value) {
-    console.log(value);
-  }
+  // handleVideoListEntryTitleClick(video) {
+  //   this.setState({currentVideo: video});
+  // }
+
+  // getYouTubeVideos(query) {
+  //   var options = {
+  //     key: this.props.API_KEY,
+  //     query: query
+  //   };
+
+  //   this.props.searchYouTube(options, (videos) => {
+  //     this.setState({
+  //       videos: videos,
+  //       currentVideo: videos[0]
+  //     });
+  //   });
+  // }
+
+  // handleSearchInputChange(value) {
+  //   console.log(value);
+  // }
 
   //TODO: swap out the React components below for the container components
   //  you wrote in the 'containers' directory.
@@ -53,18 +66,15 @@ export default class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 col-md-offset-3">
-            <Search handleSearchInputChange={this.handleSearchInputChange.bind(this)}/>
+            <SearchContainer/>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer/>
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer/>
           </div>
         </div>
       </div>
